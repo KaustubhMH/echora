34b111bb29336d38cbe737e484a01b0adb14625a
Kaustubh <kaustubhharapanahalli@gmail.com>
2018-03-05 21:07:02 +0530

    First Commit: Creating Website for ECHORA

---
+++ echorasite/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EchorasiteConfig(AppConfig):
+    name = 'echorasite'
---
+++ echorasite/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+def index(request):
+	return redirect('/home/')
+
+
+def home(request):
+	return render(request, 'echorasite/index.html')
